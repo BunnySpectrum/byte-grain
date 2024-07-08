4fc35cee66b9b37cc468af33ddfb4e517e37f738
BunnySpectrum <bunnyspectrum@gmail.com>
2024-07-07 22:49:40 -0700

    added grain struct

---
+++ src/lib/app/grain.h
@@ -0,0 +1,12 @@
+#ifndef _GRAIN_H_
+#define _GRAIN_H_
+
+#include <stdint.h>
+
+#include "app/grain_def.h"
+#include "utils/bg_codes.h"
+
+BG_CODES_e byte_to_grain(const uint8_t *byte, Grain_s *grain);
+BG_CODES_e grain_to_byte(uint8_t *byte, const Grain_s *grain);
+
+#endif
